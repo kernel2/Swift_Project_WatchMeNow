17e457a14d600b80d5eaf9289743ef4a7a17d10e
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-06-29 01:20:26 +0200

    Initial Commit

---
+++ WatchMeNow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchMeNow
+//
+//  Created by Fahraoui Zakaria on 29/06/2020.
+//  Copyright © 2020 Fahraoui Zakaria. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
