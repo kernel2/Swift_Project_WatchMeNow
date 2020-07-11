b66b937eaf7f31c042879bd59ed4b20eb1930ba7
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-07-11 21:37:14 +0200

    Add movie background card 'Landscape Image'

--- WatchMeNow/Views/MovieBackgroundCard.swift
+++ WatchMeNow/Views/MovieBackgroundCard.swift
@@ -14,13 +14,30 @@ struct MovieBackgroundCard: View {
     @ObservedObject var imageLoader = ImageLoader()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            ZStack{
+                Rectangle()
+                    .fill(Color.gray.opacity(0.3))
+                
+                if self.imageLoader.image != nil {
+                    Image(uiImage: self.imageLoader.image!)
+                        .resizable()
+                }
+            }
+            .aspectRatio(16/9, contentMode: .fit)
+            .cornerRadius(8)
+            .shadow(radius: 4)
+            Text(movie.title)
+        }
+        .lineLimit(1)
+        .onAppear {
+            self.imageLoader.loadImage(with: self.movie.backdropURL)
+        }
     }
 }
 
-
 struct MovieBackgroundCard_Previews: PreviewProvider {
     static var previews: some View {
-        MovieBackgroundCard()
+        MovieBackgroundCard(movie: Movie.stubbedMovie)
     }
 }
